6b8007b74ac94c670e3dedf8aba67f158f7ce837
Joa <leitola.joa@gmail.com>
2021-01-26 14:03:33 +0200

    .

---
+++ notes.md
@@ -0,0 +1,8 @@
+NOTES FOR FUTURE REFERENCE 
+
+npm install express cors mongoose dotenv
+npm install -g nodemon 
+// nodemon is for automatic server updates. 
+
+- running "nodemon server" starts the server.
+
---
+++ server.js
@@ -0,0 +1,47 @@
+// require express, cors, mongoose, dotenv.
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// Create app, port variables. 
+const app = express();
+const port = process.env.PORT || 5000;
+
+// App .use cors so we can run server on localhost, passing the restrictions. 
+app.use(cors());
+
+// And express.json(), to return info in JSON.
+app.use(express.json());
+
+
+
+
+// URI FOR MongoDB
+const uri = process.env.ATLAS_URI;
+// Connecting to mongoose, uri
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:true });
+// Creating a connection variable.
+const connection = mongoose.connection;
+// Once the connection is "open" => log -||-
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+
+
+
+// Routes for the server to use;
+// The server URL is https://localhost:5000. 
+// Now if you add “/exercises” or “/users” on the end it will load the endpoints defined in the corresponding router files.
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+
+
+// Makes the local server run.
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
+
