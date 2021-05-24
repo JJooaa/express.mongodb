64e6383d0c1ad511c667fa29949ad0fa96ba1ae2
Joa Leitola <leitola.joa@gmail.com>
2021-05-24 19:43:49 +0300

    .

--- routes/exercises.route.js
+++ routes/exercises.route.js
@@ -14,11 +14,11 @@ router.route('/').get((req, res) => {
 router.route('/add').post((req, res) => {
   const username = req.body.username;
   const description = req.body.description;
-  const duration = Number(req.body.duration);
+  const duration = req.body.duration;
   const date = Date.parse(req.body.date) + 10800000;
   
   const newExercise = new Exercise({
-    username,
+    username, 
     description,
     duration,
     date
@@ -50,7 +50,7 @@ router.route('/update/:id').post((req, res) => {
     .then(exercise => {
       exercise.username = req.body.username;
       exercise.description = req.body.description;
-      exercise.duration = Number(req.body.duration);
+      exercise.duration = req.body.duration;
       exercise.date = Date.parse(req.body.date) + 10800000;
 
       exercise.save()
